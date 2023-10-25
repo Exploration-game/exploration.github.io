003ba65b20445b11259231348c167a69dcc70665
lx78WyY0J5 <9j2k37st@gmail.com>
2023-10-25 16:29:01 +0200

    main website structure

---
+++ src/js/index.js
@@ -0,0 +1,74 @@
+index(); async function index() {
+    console.info("Loading index");
+
+    await styles();
+    await includes();
+
+    customPage();
+}
+async function includes() {
+    console.info("Loading includes");
+
+    await include_html("/src/html/include/head.html", "head", false);
+
+    await include_html("/src/html/include/header.html", "body", false);
+    await include_html("/src/html/include/anchor.html", "body", false);
+    await include_html("/src/html/include/content.html", "body", false);
+    await include_html("/src/html/include/footer.html", "body", false);
+}
+
+async function styles() {
+    console.info("Loading style");
+
+    await include_css("/src/css/theme.css");
+    await include_css("/src/css/font.css");
+    await include_css("/src/css/user-agent.css");
+    await include_css("/src/css/header.css");
+    await include_css("/src/css/header-navbar.css");
+    await include_css("/src/css/anchor.css");
+    await include_css("/src/css/footer.css");
+}
+
+async function customPage() {
+    console.info("Loading custom page");
+}
+
+async function include_multiple(name, area) {
+    await include_css("/src/css/" + name + ".css");
+    await include_html("/src/html/contents/" + name + ".html", area, true);
+    await include_script("/src/js/" + name + ".js");
+}
+
+async function include_html(link, query, queryOrIndex) {
+    let response = await fetch(link)
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            if (queryOrIndex) {
+                document.getElementById(query).innerHTML += data;
+            } else {
+                document.querySelector(query).innerHTML += data;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+async function include_script(url) {
+    var script = document.createElement("script");
+    script.src = url;
+    document.head.appendChild(script);
+}
+
+async function include_css(url) {
+    var head = document.getElementsByTagName('HEAD')[0];
+
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = url;
+
+    head.appendChild(link);
+}
