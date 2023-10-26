2af520b42f20bbea316b6272edf42380296e8749
lx78WyY0J5 <9j2k37st@gmail.com>
2023-10-26 16:41:09 +0200

    fix head meta tag (no dynamic SSR)

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
 index(); async function index() {
     console.info("Loading index");
 
-    //await include_html("/src/html/include/head.html", "head", false);
-
     await styles();
     await includes();
-
 }
 
 async function includes() {
@@ -14,7 +11,9 @@ async function includes() {
     await include_html("/src/html/include/header.html", "body", false);
     await include_html("/src/html/include/anchor.html", "body", false);
     await include_html("/src/html/include/content.html", "body", false);
+
     await customPage();
+    
     await include_html("/src/html/include/footer.html", "body", false);
 }
 
