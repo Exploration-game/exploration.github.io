b218ff2b4e8fd11641bec9d8a161476afdb30ba0
lx78WyY0J5 <9j2k37st@gmail.com>
2023-11-11 00:17:12 +0100

    add multi page system

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,17 @@
 index(); async function index() {
     console.info("Loading index");
-
-    await styles();
-    await includes();
+    await pages();
 }
 
 async function includes() {
+    await styles();
+
     console.info("Loading includes");
 
     await include_html("/src/html/include/header.html", "body", false);
     await include_html("/src/html/include/anchor.html", "body", false);
     await setIconTheme();
     await include_html("/src/html/include/content.html", "body", false);
-
-    await customPage();
 
     await include_html("/src/html/include/footer.html", "body", false);
 }
@@ -32,24 +30,67 @@ async function styles() {
     await include_css("/src/css/footer.css");
 }
 
-async function customPage() {
+async function pages() {
     console.info("Loading custom page");
-    await include_html("/src/html/content/welcome.html", "content", true);
 
-    await include_script("/src/js/gather.js");
-    
-    
-    await include_css("/src/css/github-update.css");
-    await include_html("/src/html/content/github-update.html", "content", true);
-    await include_script("/src/js/github-update.js");
+    if (pathNameMatchPage("") || pathNameMatchPage("index")) {
+        await includes();
 
-    await include_css("/src/css/github-commits.css");
-    await include_html("/src/html/content/github-commits.html", "content", true);
-    await include_script("/src/js/github-commits.js");
+        await include_html("/src/html/content/welcome.html", "content", true);
+        await include_script("/src/js/markdown.js");
+    }
 
-    await include_css("/src/css/github-events.css");
-    await include_html("/src/html/content/github-events.html", "content", true);
-    await include_script("/src/js/github-events.js");
+    else if (pathNameMatchPage("statistiques")) {
+        await includes();
 
-    await include_script("/src/js/markdown.js");
+        await include_script("/src/js/gather.js");
+
+        await include_css("/src/css/github-update.css");
+        await include_html("/src/html/content/github-update.html", "content", true);
+        await include_script("/src/js/github-update.js");
+
+        await include_css("/src/css/github-commits.css");
+        await include_html("/src/html/content/github-commits.html", "content", true);
+        await include_script("/src/js/github-commits.js");
+
+        await include_css("/src/css/github-events.css");
+        await include_html("/src/html/content/github-events.html", "content", true);
+        await include_script("/src/js/github-events.js");
+    }
+
+    /*
+else if (pathname === "/404.html" || pathname === "/404.html/" || pathname === "/404" || pathname === "/404/") {
+    await include_all();
+    await include("/html/contents/404Custom.html", "contentHolder", true);
+
+    await include_css("/css/404.css");
+
+    await include("/html/contents/lostTravolta.html", "contentHolder", true);
+
+    await include_multiple("github_update", "contentHolder");
+    await include_multiple("viewCount", "contentHolder");
+}
+
+else {
+    await include_all();
+    await include("/html/contents/404.html", "contentHolder", true);
+
+    await include_css("/css/404.css");
+
+    await include_multiple("github_update", "contentHolder");
+    await include_multiple("viewCount", "contentHolder");
+}
+*/
+}
+
+function pathNameMatchPage(path) {
+    var pathname = window.location.pathname;
+    pathname = pathname.replace("page/", "");
+    pathname = pathname.replace(".html", "");
+    pathname = pathname.replaceAll("/", "");
+    if (path.toLowerCase() === pathname) {
+        return true;
+        console.log("Loading : " + pathname);
+    }
+    return false;
 }
