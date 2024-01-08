ecf48ee3be9702ba14a36a57b5d451fbf2820cf6
lx78WyY0J5 <9j2k37st@gmail.com>
2024-01-09 00:15:02 +0100

    new issue manager

--- src/js/issues.js
+++ src/js/issues.js
@@ -7,9 +7,11 @@ async function start(orgs) {
 async function getIssues(repo, orgs) {
     var x = await gather('https://api.github.com/repos/' + orgs + '/' + repo + '/issues');
     for (var i in x) {
+        console.log(i);
         var issueURL = await getValue(x[i], "html_url");
+        var issueNumber = await getValue(x[i], "number");
+        var issueLabels = await gather("https://api.github.com/repos/" + orgs + "/" + repo + "/issues/" + issueNumber + "/labels");
         var issueTitle = await getValue(x[i], "title");
-
         var user = await getValue(x[i], "user");
         var userLogin = await getValue(user, "login");
         var userAvatar = await getValue(user, "avatar_url");
@@ -18,7 +20,7 @@ async function getIssues(repo, orgs) {
         var dateUpdate = await getValue(x[i], "updated_at");
         var body = await getValue(x[i], "body");
 
-        await display(issueURL, repo, issueTitle, userLogin, userAvatar, state, dateUpdate, body);
+        await display(issueURL, issueLabels, repo, issueTitle, userLogin, userAvatar, state, dateUpdate, body);
     }
     if (x === null || x === undefined || x.length <= 0) { } else {
         var root = document.getElementById("github_issue");
@@ -34,47 +36,79 @@ async function getRepo(orgs) {
     }
 }
 
-async function display(issueURL, repo, issueTitle, userLogin, userAvatar, state, dateUpdate, body) {
+async function display(issueURL, issueLabels, repo, issueTitle, userLogin, userAvatar, state, dateUpdate, body) {
     var root = document.getElementById("github_issue");
 
     var div = document.createElement('div');
-    div.style = "display: inline-block;margin:20px;max-width:30vw;max-height:30vh;overflow-x:auto;"
+    div.className = "div";
 
     var img = document.createElement('img');
     img.src = userAvatar;
-    img.style = "max-width: 100px;max-height:100px;border-radius:25px;";
+    img.className = "profile";
     div.appendChild(img);
 
-    var img2 = document.createElement('img');
+    /* var img2 = document.createElement('img');
     if (state === "open") {
         img2.src = "/assets/svg/issue.svg";
     } else {
         img2.src = "/assets/svg/issue-closed.svg";
     }
-    img2.className = "svg";
-    img2.style = "max-width: 50px;max-height:50px;padding-top:10px;padding-bottom:10px;padding-left:15px;";
-    div.appendChild(img2);
+    img2.className = "svg logoissue";
+    div.appendChild(img2); */
 
     var a = document.createElement('a');
     a.textContent = userLogin;
     a.href = "https://github.com/" + userLogin;
-    a.style = "display:block;";
+    a.className = "username";
     div.appendChild(a);
 
     var a2 = document.createElement('a');
     a2.href = issueURL;
     a2.textContent = issueTitle;
+    a2.className = "issuelink";
     div.appendChild(a2);
+
+    for (var i in issueLabels) {
+        var labelName = await getValue(issueLabels[i], "name");
+        //var labelColor = await getValue(issueLabels[i], "color");
+        var labelDescription = await getValue(issueLabels[i], "description");
+        console.log(labelDescription);
+        var img3 = document.createElement('p');
+        if (labelName === "wontfix") {
+            img3.textContent = "🙅🔨";
+        } else if (labelName === "question") {
+            img3.textContent = "☝💬";
+        } else if (labelName === "invalid") {
+            img3.textContent = "😡🔧";
+        } else if (labelName === "help wanted") {
+            img3.textContent = "🔥🆘";
+        } else if (labelName === "good first issue") {
+            img3.textContent = "🎓🎒";
+        } else if (labelName === "enhancement") {
+            img3.textContent = "✨";
+        } else if (labelName === "duplicate") {
+            img3.textContent = "🎭♻";
+        } else if (labelName === "documentation") {
+            img3.textContent = "📚💾";
+        } else if (labelName === "bug") {
+            img3.textContent = "🐛🐞";
+        } else {
+            img3.textContent = "❌🔐";
+        }
+        img3.className = "labelissue";
+        div.appendChild(img3);
+    }
 
     var p = document.createElement('p');
     const date1 = new Date(dateUpdate);
     var date2 = date1.toLocaleString();
     p.textContent = date2;
+    p.className = "date";
     div.appendChild(p);
 
     var p3 = document.createElement('p');
     p3.textContent = repo;
-    p3.style = "padding-bottom:10px;";
+    p3.className = "repo";
     div.appendChild(p3);
 
     var p2 = document.createElement('p');
